b83717f440f44d0cef37e3bd66308b9e7d104d05
CloudSearch <yunfeng97@gmail.com>
2015-02-24 22:40:41 -0500

    initial

---
+++ api/controllers/Review/ReviewController.js
@@ -0,0 +1,14 @@
+/**
+ * Review/ReviewController
+ *
+ * @description :: Server-side logic for managing review/reviews
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+    createNewReview:    function(){
+
+    }
+	
+};
+
