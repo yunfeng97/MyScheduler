17029e9fccdd5ad4e1a29f1ca48384d4440ab515
CloudSearch <yunfeng97@gmail.com>
2015-02-25 20:46:34 -0500

    initial

---
+++ api/controllers/Calendar/CustomerCalendarController.js
@@ -0,0 +1,11 @@
+/**
+ * Calendar/CustomerCalendarController
+ *
+ * @description :: Server-side logic for managing calendar/customercalendars
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
