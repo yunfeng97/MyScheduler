194432c842f2e13d82443d08f34228ca2ada7ea3
CloudSearch <yunfeng97@gmail.com>
2015-03-02 22:44:52 -0500

    initial

---
+++ api/models/CalendarTemplate-copy.js
@@ -0,0 +1,37 @@
+/**
+* CalendarTemplate.js
+*
+* @description :: CalendarTemplate model.
+ * A weekly calendar will be created based on this template.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+module.exports = {
+
+  attributes: {
+      ownerId:          'string',
+      serviceId:        'string',
+      serviceName:      'string',
+      serviceDesc:      'string',
+      openOnHoliday:    'boolean',
+      //businessHours:    'array'
+      mon:              'json',
+      tue:              'json',
+      wed:              'json',
+      thu:              'json',
+      fri:              'json',
+      sat:              'json',
+      sun:              'json'
+      /*
+     
+      {
+        closed:         'boolean',
+        startTime:      'datetime',
+        endTime:        'datetime',
+        breakTimeFrom:  'datetime',
+        breakTimeTo:    'datetime'
+      }
+       */
+  }
+};
+
