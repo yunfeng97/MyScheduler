a464a25446bc7be3a84d36db61ea312cfb1a7d52
CloudSearch <yunfeng97@gmail.com>
2015-04-09 22:37:46 -0400

    initial

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,22 @@
 /**
  * Created by yunfeng on 08/04/15.
  */
-angular.module('myscheduler', ["ngRoute"])
+angular.module('Myscheduler', ["ngRoute"])
     .config(['$routeProvider',
         function($routeProvider) {
             $routeProvider.
-                when('/phones', {
-                    templateUrl: '/homepage.html',
-                    controller: 'PersonnelCtrl'
+                when('/phone', {
+                    templateUrl: '../views/homepage.html',
+                    controller: 'MySchedulerController'
                 }).
                 when('/', {
-                    templateUrl: '/homepage.ejs',
-                    controller: 'PersonnelCtrl'
+                    templateUrl: '../views/homepage.ejs',
+                    controller: 'MySchedulerController'
                 }).
                 otherwise({
-                    redirectTo: '/phones'
+                    redirectTo: '../views/phones'
                 });
         }])
-    .controller('PersonnelCtrl', ['$scope', function($scope){
+    .controller('MySchedulerController', ['$scope', function($scope){
         $scope.personnel = [{id:'1', name:"David"}, {id:'2', name:"Jon"}, {id:'3', name:"Tim"}, {id:'4', name:"Don"}];
-    }])
-    .controller('SubjectCtrl', ['$scope', function($scope){
-        $scope.subjects = ['100101', '100201', '100301'];
-    }])
-    .controller('VisitCtrl', ['$scope', function($scope){
-        $scope.visits = ["Enr", "Xab", "Conv"];
     }]);
